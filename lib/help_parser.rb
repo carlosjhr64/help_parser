92cda85f52c1f01cbed0c5c35b9edcf6d3e3fccb
carlosjhr64 <carlosjhr64@gmail.com>
2018-03-01 09:32:35 -0800

    minor version bump 6.3

--- lib/help_parser.rb
+++ lib/help_parser.rb
@@ -11,7 +11,7 @@ require_relative './help_parser/options'
 require_relative './help_parser/macros'
 
 module HelpParser
-  VERSION = '6.2.0'
+  VERSION = '6.3.0'
 
   def self.[](
     version = nil,
